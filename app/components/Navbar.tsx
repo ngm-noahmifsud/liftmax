c1def592e1c0aacf2be014a6c902609b1bc2d297
ngm-noahmifsud <nmifsud.ngm@gmail.com>
2025-11-13 12:53:22 +1100

    style(Navbar): implement basic navbar styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,10 +7,20 @@ type NavbarProps = {
 
 function Navbar({ title }: NavbarProps) {
   return (
-    <nav>
-      <h1>{title}</h1>
-      <Link href="/">Dashboard</Link>
-      <Link href="/history">History</Link>
+    <nav className="w-screen bg-slate-900">
+      <div className="p-2">
+        <h2>{title}</h2>
+      </div>
+      <div
+        className="
+        flex justify-between md:justify-start
+        px-4 py-2 [&>*]:pr-4
+        "
+      >
+        <Link href="/">Dashboard</Link>
+        <Link href="/history">History</Link>
+        <Link href="/exercises">Exercises</Link>
+      </div>
     </nav>
   );
 }
---
+++ app/exercises/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Exercises() {
+  return (
+    <div>
+      <Navbar title="Exercises" />
+    </div>
+  );
+}
+
+export default Exercises;
